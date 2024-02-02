c3818850d637b93903a832de11701eae414b19e0
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2024-02-02 16:10:56 +0800

    16:10 - 2/2/2024
    project started

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Neil and Roselyn Wedding</title>
+  <meta charset="utf-8">
+  <meta name="Neil and Roselyn are Getting Married" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" media="screen" href="includes/styles.css">
+  <link rel="stylesheet" media="screen" href="includes/responsive.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/d25a837557.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <section id="header" class="sections">
+        <div class="containers">
+            <div class="logo-cont">
+                <img class="logo" src="includes/images/gg2.png" alt="logo">
+            </div>
+            <div class="header-list">
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                    <li>Lorem Ipsum</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section id="hero" class="sections">
+        <div class="containers">
+            <h1>Lorem Ipsum</h1>
+        </div>
+    </section>
+</body>
+
+</html>
