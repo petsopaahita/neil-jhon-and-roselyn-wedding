c425efc45011420cbf1b06fd3fef34a27e8b1411
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2024-02-05 17:33:03 +0800

    2/5/2024 - 17:32
    Added new sections

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Neil and Roselyn Wedding</title>
+  <title>Neil Jhon and Roselyn Wedding</title>
   <meta charset="utf-8">
-  <meta name="Neil and Roselyn are Getting Married" content="" />
+  <meta name="Neil Jhon and Roselyn are Getting Married" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link rel="stylesheet" media="screen" href="includes/styles.css">
   <link rel="stylesheet" media="screen" href="includes/responsive.css">
+  <link rel="stylesheet" media="screen" href="includes/scripts.js">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -14,6 +15,7 @@
   <script src="https://kit.fontawesome.com/d25a837557.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/d25a837557.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -24,23 +26,120 @@
             </div>
             <div class="header-list">
                 <ul>
-                    <li>Home</li>
-                    <li>Our Story</li>
-                    <li>RSVP</li>
+                    <li><a href="" data-section="hero-section">Home</a></li>
+                    <li><a href="" data-section="story-section">Our Story</a></li>
+                    <li><a href="" data-section="details-section">Details</a></li>
+                    <li><a href="" data-section="rsvp-section">RSVP</a></li>
                 </ul>
             </div>
         </div>
     </section>
     <section id="hero" class="sections">
         <div class="containers">
-            <h4>02 - 02 - 2024</h4>
-            <h1>Neil and Roselyn</h1>
-            <h4>are tying the knot</h4>
+            <h3 class="color-primary">02 - 02 - 2024</h4>
+            <h1>Neil Jhon and Roselyn</h1>
+            <h3 class="color-primary">are getting married</h4>
+            <button class="rsvp-btn">
+                <span>RSVP</span>
+            </button>
         </div>
     </section>
-    <section id="hero" class="sections">
+    <section id="story" class="sections">
         <div class="containers">
-            <h1>Lorem Ipsum</h1>
+            <div class="spacer"></div>
+            <div class="section-tiles">
+                <h2 class="color-white">OUR STORY</h2>
+            </div>
+            <div class="spacer"></div>
+                <div class="two-col-cont">
+                    <div class="two-col">
+                        <img src="includes/images/pexels-j-carter-254069.jpg" alt="">
+                    </div>
+                    <div class="two-col color-white">
+                        <div class="text-box">
+                            <span>"We met 9 years ago in London for the first time. Dave was in a supermarket, in the frozen aisle trying to figure out what vegan ice cream is and a girl walks up and says: ‘Hey, do you know where to find vegan ice cream?’. As a couple we've been are together for 8 years now & we’re getting married next year."</span>
+                            <br><br>
+                            <span>- Kapitbahay</span>
+                        </div>
+                    </div>
+                </div>
+            <div class="spacer"></div>
+        </div>
+    </section>
+    <section id="details">
+        <div class="containers">
+            <div class="two-col-cont">
+                <div class="two-col">
+                    <div class="spacer-2"></div>
+                    <div class="text-box">
+                        <h2 class="text-align-center color-secondary">Ceremony</h2>
+                        <div class="spacer-2"></div>
+                        <p class="text-align-center color-secondary">The wedding ceremony will take place at the skies while skydiving.</p>
+                        <br>
+                        <p class="text-align-center color-secondary">The 2nd Christian Cemetery, Lyustdorfs'ka Road 6 65000, Odessa Oblast Ukraine</p>
+                        <br>
+                        <p class="text-align-center color-secondary">1:00AM — 2:30AM</p>
+                    </div>
+                    <div class="spacer-2"></div>
+                </div>
+                <div class="two-col">
+                    <div class="spacer-2"></div>
+                    <div class="text-box">
+                        <h2 class="text-align-center color-secondary">Reception</h2>
+                        <div class="spacer-2"></div>
+                        <p class="text-align-center color-secondary">Join our underwater reception in the beautiful seas of Banago.</p>
+                        <br>
+                        <p class="text-align-center color-secondary">Prk. Kitahanon, Brgy. Banago, Bacolod City</p>
+                        <br>
+                        <p class="text-align-center color-secondary">4:00AM — 4:10AM</p>
+                    </div>
+                    <div class="spacer-2"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="rsvp-section">
+        <div class="containers">
+            <div class="spacer"></div>
+            <div class="section-tiles">
+                <h2 class="color-white">TIME TO TIE THE KNOT</h2>
+            </div>
+            <div class="spacer"></div>
+            <div class="text-box">
+                <h4 class="color-white">We want to celebrate the new chapter in our lives with you,</h4>
+                <h4 class="color-white">so please join us on this very special day.</h4>
+            </div>
+            <div class="spacer"></div>
+            <button class="rsvp-btn">
+                <span>RSVP</span>
+            </button>
+            <div class="spacer"></div>
+        </div>
+    </section>
+    <section id="footer">
+        <div class="containers">
+            <div class="spacer"></div>
+            <div class="two-col-cont">
+                <div class="two-col">
+                    <div class="text-box">
+                        <h5 class="color-white">Have Questions?</h5>
+                        <div class="spacer-2"></div>
+                        <h5 class="color-white">Tanong mo sa pagong: 123-456-789</h5>
+                        <br>
+                        <h5 class="color-white">Sumbong mo kay Tulfo: 123-456-789</h5>
+                        <br>
+                        <h5 class="color-white">Email: sekretoh@gmail</h5>
+                    </div>
+                </div>
+                <div class="two-col center-items">
+                    <div class="social-icon-cont">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="spacer"></div>
         </div>
     </section>
 </body>
