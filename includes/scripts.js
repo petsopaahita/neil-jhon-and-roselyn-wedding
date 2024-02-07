4392d395772904edf4e31bafe9b99b4c52660c18
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2024-02-07 17:58:41 +0800

    2/7/2024 - 17:58
    added popups

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,3 +1,68 @@
+const rsvpBtn1 = document.querySelector('#rsvp-btn-1');
+const rsvpBtn2 = document.querySelector('#rsvp-btn-2');
+const rsvpPopup = document.querySelector('#rsvp-popup-main');
+const main = document.querySelector('#main');
+const closeBtn = document.querySelector('#close-btn');
+const brgrMenu = document.querySelector('#hamburger-menu');
+const menuPopup = document.querySelector('#menu-popup');
+const closeBtn2 = document.querySelector('#close-btn-2');
+const menuPopupContainer = document.querySelector('#menu-popup-container');
+
+window.onload = function() {
+  const heroSection = document.querySelector('#hero');
+  heroSection.classList.add('animate');
+}
+
+rsvpBtn1.addEventListener('click', () => {
+  rsvpPopup.classList.remove('hide');
+  rsvpPopup.classList.add('show');
+  main.classList.add('blur');
+});
+
+rsvpBtn2.addEventListener('click', () => {
+  rsvpPopup.classList.remove('hide');
+  rsvpPopup.classList.add('show');
+  main.classList.add('blur');
+});
+
+closeBtn.addEventListener('click', () => {
+  rsvpPopup.classList.remove('show');
+  rsvpPopup.classList.add('hide');
+  main.classList.remove('blur');
+});
+
+rsvpPopup.addEventListener('click', (e) => {
+  if (e.target === rsvpPopup) {
+    e.preventDefault();
+    rsvpPopup.classList.add('hide');
+    rsvpPopup.classList.remove('show');
+    main.classList.remove('blur');
+  }
+});
+
+// ----------------------------------------------
+
+brgrMenu.addEventListener('click', () => {
+  menuPopup.classList.remove('hide');
+  menuPopup.classList.add('show');
+  main.classList.add('blur-2');
+});
+
+closeBtn2.addEventListener('click', () => {
+  menuPopup.classList.remove('show');
+  menuPopup.classList.add('hide');
+  main.classList.remove('blur-2');
+});
+
+menuPopupContainer.addEventListener('click', (e) => {
+  if (e.target === menuPopupContainer) {
+    e.preventDefault();
+    menuPopup.classList.add('hide');
+    menuPopup.classList.remove('show');
+    main.classList.remove('blur-2');
+  }
+});
+
 $('#story-link').click(function(e) {
     e.preventDefault();
     $('html, body').animate({
--- index.php
+++ index.php
@@ -11,12 +11,12 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/d25a837557.js" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
+<section id="main">
     <section id="header" class="sections">
         <div class="containers">
             <div class="logo-cont">
@@ -30,17 +30,17 @@
                     <li><a id="rsvp-link">RSVP</a></li>
                 </ul>
             </div>
-            <div class="hamburger-menu">
+            <div class="hamburger-menu" id="hamburger-menu">
                 <i class="fa-solid fa-bars"></i>
             </div>
         </div>
     </section>
-    <section id="hero" class="sections">
+    <section id="hero" class="sections animate">
         <div class="containers">
             <h3 class="color-primary">02 - 02 - 2024</h4>
             <h1>Neil Jhon and Roselyn</h1>
             <h3 class="color-primary">are getting married</h4>
-            <button class="rsvp-btn">
+            <button class="rsvp-btn" id="rsvp-btn-1">
                 <span>RSVP</span>
             </button>
         </div>
@@ -70,7 +70,7 @@
                         <div class="text-box">
                             <span>"We met 9 years ago in London for the first time. Dave was in a supermarket, in the frozen aisle trying to figure out what vegan ice cream is and a girl walks up and says: ‘Hey, do you know where to find vegan ice cream?’. As a couple we've been are together for 8 years now & we’re getting married next year."</span>
                             <br><br>
-                            <span>- Cashier sa SM</span>
+                            <span>- Tambay sa kanto</span>
                         </div>
                     </div>
                     <div class="two-col">
@@ -101,7 +101,7 @@
                     <div class="text-box">
                         <h2 class="text-align-center color-secondary">Reception</h2>
                         <div class="spacer-2"></div>
-                        <p class="text-align-center color-secondary">Join our underwater reception in the beautiful seas of Banago.</p>
+                        <p class="text-align-center color-secondary">Join our underwater reception in the beautiful seas of Banago. (P.S. Pls bring your own scuba diving gear.)</p>
                         <br>
                         <p class="text-align-center color-secondary">Prk. Kitahanon, Brgy. Banago, Bacolod City</p>
                         <br>
@@ -124,7 +124,7 @@
                 <h4 class="color-white">so please join us on this very special day.</h4>
             </div>
             <div class="spacer"></div>
-            <button class="rsvp-btn">
+            <button class="rsvp-btn" id="rsvp-btn-2">
                 <span>RSVP</span>
             </button>
             <div class="spacer"></div>
@@ -138,51 +138,79 @@
                     <div class="text-box">
                         <h5 class="color-white">Have Questions?</h5>
                         <div class="spacer-2"></div>
-                        <h5 class="color-white">Tanong mo sa pagong: 123-456-789</h5>
+                        <h5 class="color-white">Tanong mo sa pagong: <a href="tel:123-456-789">123-456-789</a></h5>
                         <br>
-                        <h5 class="color-white">Sumbong mo kay Tulfo: 123-456-789</h5>
+                        <h5 class="color-white">Sumbong mo kay Tulfo: <a href="tel:123-456-789">123-456-789</a></h5>
                         <br>
-                        <h5 class="color-white">Email: sekretoh@gmail</h5>
+                        <h5 class="color-white">Email: <a href="mailto:sekretoh@gmail">sekretoh@gmail</a></h5>
                     </div>
                 </div>
                 <div class="two-col center-items">
                     <div class="social-icon-cont">
-                        <i class="fa-brands fa-facebook-f"></i>
-                        <i class="fa-brands fa-twitter"></i>
-                        <i class="fa-brands fa-instagram"></i>
+                        <a href="https://www.facebook.com/" target="blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/" target="blank"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/" target="blank"><i class="fa-brands fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
             <div class="spacer"></div>
         </div>
     </section>
+</section>
     
-    <section id="rsvp-popup-main" >
-        <div class="rsvp-popup">
-            <div class="spacer"></div>
-            <h3 class="color-primary text-align-center">Lorem Ipsum Lorem Ipsum</h3>
-            <div class="spacer-2"></div>
-            <form id="rsvp-form">
-                <div class="two-col-cont">
-                    <div class="two-col">
-                        <label for="first-name" class="color-primary">First Name:</label>
-                        <label for="last-name" class="color-primary">Last Name:</label>
-                        <label for="email" class="color-primary">Email:</label>
-                    </div>
-                    <div class="two-col">
-                        <input type="text" id="first-name" name="first-name" required>
-                        <input type="text" id="last-name" name="last-name" required>
-                        <input type="email" id="email" name="email">
-                    </div>
+<section id="rsvp-popup-main" >
+    <div class="rsvp-popup">
+        <i class="fa-solid fa-xmark" id="close-btn"></i>
+        <div class="spacer"></div>
+        <h3 class="color-white text-align-center">Lorem Ipsum Lorem Ipsum</h3>
+        <div class="spacer-2"></div>
+        <form id="rsvp-form">
+            <div class="two-col-cont">
+                <div class="two-col">
+                    <label for="first-name" class="color-white">First Name:</label>
                 </div>
-                <div class="spacer-2"></div>
-                <button class="rsvp-btn">
-                    <span>RSVP</span>
-                </button>
-            </form>
-            <div class="spacer"></div>
+                <div class="two-col">
+                    <input type="text" id="first-name" name="first-name" required>
+                </div>
+            </div>
+            <div class="two-col-cont">
+                <div class="two-col">
+                    <label for="first-name" class="color-white">Last Name:</label>
+                </div>
+                <div class="two-col">
+                    <input type="text" id="last-name" name="last-name" required>
+                </div>
+            </div>
+            <div class="two-col-cont">
+                <div class="two-col">
+                    <label for="email" class="color-white">Email:</label>
+                </div>
+                <div class="two-col">
+                    <input type="text" id="email" name="email" required>
+                </div>
+            </div>
+            <div class="spacer-2"></div>
+            <button class="submit-btn">
+                <span>I will be there!</span>
+            </button>
+        </form>
+        <div class="spacer"></div>
+    </div>
+</section>
+
+<section id="menu-popup">
+    <div class="containers" id="menu-popup-container">
+        <i class="fa-solid fa-xmark" id="close-btn-2"></i>
+        <div class="menu-popup-list">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a id="story-link">Our Story</a></li>
+                <li><a id="details-link">Details</a></li>
+                <li><a id="rsvp-link">RSVP</a></li>
+            </ul>
         </div>
-    </section>
+    </div>
+</section>
 
 <!-- scripts -->
 <script src="includes/scripts.js"></script>
